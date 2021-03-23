5c8e9ecb853251337c94073032d39d50c0da005a
Kyle Opperman <kyleopperman1@gmail.com>
2021-03-23 12:46:32 +0100

    Update radius value on selection

--- Radius Changer/code.js
+++ Radius Changer/code.js
@@ -6,34 +6,34 @@
 
 figma.showUI(__html__)
 
-
 // send Message to UI 
-function sendRadiusInfo(RadiusValues){
+const sendRadiusInfo = RadiusValues => {
   figma.ui.postMessage({
     //type: 'selectionchange',
     RadiusValues,
   }) 
-
- }
+}
 
 //find and listen for RECTANGLE nodes
 figma.on('selectionchange', () => {
-  //Select
-  if (figma.currentPage.selection.length >= 1){
-    const selection = figma.currentPage.selection.filter(
-      node => node.type === "RECTANGLE")
-      
-      //check selected radius
-      console.log(figma.currentPage.selection[0].cornerRadius)
-      //check what type of object it is, rect, circle etc 
-      console.log(figma.currentPage.selection)
-    //find all radius values
-    const RadiusValues = selection.map(node => node.cornerRadius)
-    //passes radius values to a function that sends a message to fron ui 
-    sendRadiusInfo(RadiusValues)
-      
-  } 
+  // If nothing is selected, return
+  if (!figma.currentPage.selection.length) return
+
+  // Constants for type/s & radius
+  const RECT = "RECTANGLE"
+  const RADIUS = 24
+
+  const selection = figma.currentPage.selection
+    .filter(node => node.type === RECT)
+
+  if (selection.length) {
+    selection.forEach(rect => {
+      rect.cornerRadius = RADIUS
+    })
+  }
+
+  const radiusValues = selection.map(node => node.cornerRadius)
+  //passes radius values to a function that sends a message to fron ui 
+  sendRadiusInfo(radiusValues)
 })
-
-
 
